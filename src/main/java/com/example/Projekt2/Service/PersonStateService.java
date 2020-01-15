f2ce86874936a651420ed1751c4e048c1bead739
Eugene <zhekadeni98@mail.ru>
2020-01-15 22:26:53 +0100

    SignUp/SignIn

--- src/main/java/com/example/Projekt2/Service/PersonStateService.java
+++ src/main/java/com/example/Projekt2/Service/PersonStateService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class PersonStateService {
     @Autowired
