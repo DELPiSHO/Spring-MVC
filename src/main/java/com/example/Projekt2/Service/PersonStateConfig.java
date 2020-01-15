68c00838ca91770fff5177096b0526fae3f02ab0
Eugene <zhekadeni98@mail.ru>
2020-01-15 22:26:34 +0100

    SignUp/SignIn

--- src/main/java/com/example/Projekt2/Service/PersonStateConfig.java
+++ src/main/java/com/example/Projekt2/Service/PersonStateConfig.java
@@ -5,7 +5,6 @@ import com.example.Projekt2.Domain.PersonState;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class PersonStateConfig {
     @Bean
